9ae5bb3b0c63c4d692f20bcfffd64617b9a2e486
holms2000 <holms_2000@mail.ru>
2025-12-22 22:45:50 +0300

    int

--- Example01.py
+++ Example01.py
@@ -1,2 +1,4 @@
-value = input("Введите целочисленное значение")
+value = int(input("Введите целочисленное значение"))
 print(value*value)
+git config --global user.email "holms_2000@mail.ru"
+git config --global user.name "holms2000"
